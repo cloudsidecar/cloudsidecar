57e9d3d0d582c9e7eb38ce240244e22d3cd6afba
Lawrence Finn <laf2019@columbia.edu>
2019-03-06 23:31:15 -0500

    chunk test

--- pkg/aws/handler/s3/object/object_handler_test.go
+++ pkg/aws/handler/s3/object/object_handler_test.go
@@ -1,20 +1,125 @@
 package object
 
 import (
+	"bytes"
 	"cloud.google.com/go/storage"
 	"context"
+	"fmt"
 	"github.com/golang/mock/gomock"
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	s3_handler "sidecar/pkg/aws/handler/s3"
 	"sidecar/pkg/mock"
+	"strings"
 	"testing"
 	"time"
 )
 
+func TestHandler_PutParseInputChunked(t *testing.T) {
+	valueMap := map[string]string {"bucket": "boops", "key" : "mykey"}
+	testUrl, _ := url.ParseRequestURI("http://localhost:3450/beh?list-type=2&prefix=boo&delimiter=%2F&encoding-type=url")
+	bodyString := fmt.Sprintf(
+		"%s\r\n%s\r\n%s\r\n%s\r\n%s\r\n",
+		"B;chunk-signature=meowmeowmeow",
+		"thisisten\r\n",
+		"5;chink-signature=mooo",
+		"12345",
+		"0;chunk-signature=nope",
+		)
+	body := ioutil.NopCloser(bytes.NewReader([]byte(bodyString)))
+	req := &http.Request{
+		URL: testUrl,
+		Body: body,
+		Header: make(http.Header),
+	}
+	req = mux.SetURLVars(req, valueMap)
+	req.Header.Set("Content-Type", "text")
+	req.Header.Set("x-amz-content-sha256", "STREAMING-AWS4-HMAC-SHA256-PAYLOAD")
+	handler := New(nil)
+	result, _ := handler.PutParseInput(req)
+	outputBody, _ := ioutil.ReadAll(result.Body)
+	assert.Equal(t, "thisisten\r\n12345", string(outputBody))
+}
 
+func TestHandler_PutParseInput(t *testing.T) {
+	valueMap := map[string]string {"bucket": "boops", "key" : "mykey"}
+	testUrl, _ := url.ParseRequestURI("http://localhost:3450/beh?list-type=2&prefix=boo&delimiter=%2F&encoding-type=url")
+	body := ioutil.NopCloser(bytes.NewReader([]byte("enjoy my body")))
+	req := &http.Request{
+		URL: testUrl,
+		Body: body,
+		Header: make(http.Header),
+	}
+	req = mux.SetURLVars(req, valueMap)
+	req.Header.Set("Content-Type", "text")
+	handler := New(nil)
+	result, _ := handler.PutParseInput(req)
+	if *result.Bucket != "boops" {
+		t.Error("Bucket should be boops")
+	}
+	if *result.Key != "mykey" {
+		t.Error("Key should be mykey")
+	}
+	outputBody, _ := ioutil.ReadAll(result.Body)
+	assert.Equal(t, "enjoy my body", string(outputBody))
+	assert.Equal(t, "text", *result.ContentType)
+}
+
+func todo_TestHandler_GetHandle(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	bucketMock := s3_handler.NewMockGCPBucket(ctrl)
+	clientMock := s3_handler.NewMockGCPClient(ctrl)
+	objectMock := s3_handler.NewMockGCPObject(ctrl)
+	writerMock := mock.NewMockResponseWriter(ctrl)
+	ctx := context.Background()
+	s3Handler := &s3_handler.Handler{
+		GCPClient: clientMock,
+		GCPClientToBucket: func(bucket string, client s3_handler.GCPClient) s3_handler.GCPBucket {
+			return bucketMock
+		},
+		GCPBucketToObject: func(name string, bucket s3_handler.GCPBucket) s3_handler.GCPObject {
+			return objectMock
+		},
+		Context: &ctx,
+	}
+	valueMap := map[string]string {"bucket": "boops", "key" : "mykey"}
+	testUrl, _ := url.ParseRequestURI("http://localhost:3450/boops/mykey")
+	req := &http.Request{
+		URL: testUrl,
+	}
+	req = mux.SetURLVars(req, valueMap)
+	attrs := &storage.ObjectAttrs{
+		Bucket: "boops",
+		Name: "mykey",
+		Size: 123,
+	}
+	fakeReader := strings.NewReader("this is my file")
+	objectMock.EXPECT().Attrs(ctx).Return(attrs, nil)
+	objectMock.EXPECT().NewReader(ctx).Return(fakeReader, nil)
+	handler := New(s3Handler)
+	handler.GetHandle(writerMock, req)
+}
+
+func TestHandler_GetParseInput(t *testing.T) {
+	valueMap := map[string]string {"bucket": "boops", "key" : "mykey"}
+	testUrl, _ := url.ParseRequestURI("http://localhost:3450/beh?list-type=2&prefix=boo&delimiter=%2F&encoding-type=url")
+	req := &http.Request{
+		URL: testUrl,
+	}
+	req = mux.SetURLVars(req, valueMap)
+	handler := New(nil)
+	result, _ := handler.GetParseInput(req)
+	if *result.Bucket != "boops" {
+		t.Error("Bucket should be boops")
+	}
+	if *result.Key != "mykey" {
+		t.Error("Key should be mykey")
+	}
+}
 
 func TestHandler_HeadHandle(t *testing.T) {
 	ctrl := gomock.NewController(t)
