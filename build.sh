6594febb4e5b06e72ebbf7b7c620027bc1e59678
Lawrence Finn <laf2019@columbia.edu>
2019-07-21 21:32:41 -0400

    Fixing flakiness with multipart Catching errors better Forcing a close sooner on uploader to flush data

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-env GOOS=linux GOARCH=amd64 go build -o cloud_sidecar_0.0.7_amd64 main.go
-env GOOS=darwin GOARCH=amd64 go build -o cloud_sidecar_0.0.7_darwin main.go
+env GOOS=linux GOARCH=amd64 go build -o cloud_sidecar_0.0.10_amd64 main.go
+env GOOS=darwin GOARCH=amd64 go build -o cloud_sidecar_0.0.10_darwin main.go
 
